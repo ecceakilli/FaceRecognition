88334dabb768ea5bfe9a9a17d30f6a47e05c873c
eceImac <ece.akilli@icisleri.gov.tr>
2022-02-16 16:22:48 +0300

    parmak izi-yüz tanıma

--- FaceRecognition/ViewController.swift
+++ FaceRecognition/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func singInClicked(_ sender: Any) {
+        
+        //dogrulama işlemleri için kullanılan obje
+        let authContext = LAContext()
+              
+              var error: NSError?
+              
+              if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                  
+                  authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it you?") { (success, error) in
+                      if success == true {
+                          //successful auth
+                          DispatchQueue.main.async {
+                              self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+                          }
+                      } else {
+                          DispatchQueue.main.async {
+                              self.label.text = "Error!"
+                          }
+                      }
+                  }
+                  
+                  
+              }
+    }
 }
 
